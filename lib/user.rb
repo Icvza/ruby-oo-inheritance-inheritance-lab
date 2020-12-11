e55b060268157f64021f281f3fe8a1a843c8247a
Isaac <deisaacalejandro@gmail.com>
2020-12-11 16:22:54 -0500

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,13 @@
+require'pry'
+
+
+class User
+    attr_accessor :first_name, :last_name
+
+    def initialize(first_name = 0, last_name = 0)
+        @first_name = first_name
+        @last_name = last_name
+    end
+
+
+end
